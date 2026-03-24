859511e27b97edf6cfaf415a9a5091009ce9764b
Dinur Pradipta <dinurm.pradipta@gmail.com>
2026-03-24 12:24:33 +0800

    add cloudflare setting

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = "edge";
+
 import React, { useState, use, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
